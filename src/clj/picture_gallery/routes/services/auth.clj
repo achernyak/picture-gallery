90d4ac0285b38e01dcaf8d7832fad11c10d2bb80
achernyak <artemchernyak@gmail.com>
2016-11-07 06:53:08 -0600

    login backend

--- src/clj/picture_gallery/routes/services/auth.clj
+++ src/clj/picture_gallery/routes/services/auth.clj
@@ -5,6 +5,8 @@
             [picture-gallery.validation :refer [registration-errors]]
             [ring.util.http-response :as response]))
 
+;; Registration
+
 (defn handle-registration-error [e]
   (if (and
        (instance? java.sql.SQLException e)
@@ -33,3 +35,29 @@
           (assoc :session (assoc session :identity (:id user))))
       (catch Exception e
         (handle-registration-error e)))))
+
+;; Login
+
+(defn decode-auth [encoded]
+  (let [auth (second (.split encoded " "))]
+    (-> (.decode (java.util.Base64/getDecoder) auth)
+        (String. (java.nio.charset.Charset/forName "UTF-8"))
+        (.split ":"))))
+
+(defn authenticate [[id pass]]
+  (when-let [user (db/get-user {:id id})]
+    (when (hashers/check pass (:pass user))
+      id)))
+
+(defn login! [{:keys [session]} auth]
+  (if-let [id (authenticate (decode-auth auth))]
+    (-> {:result :ok}
+        (response/ok)
+        (assoc :session (assoc session :identity id)))
+    (response/unauthorized {:result :unauthorized
+                            :message "login failure"})))
+
+(defn logout! []
+  (-> {:result :ok}
+      (response/ok)
+      (assoc :session nil)))
