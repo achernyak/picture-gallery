3cd8a3d7e24e00bb505d10d21b211eaec545b4a1
achernyak <artemchernyak@gmail.com>
2016-11-09 06:08:46 -0600

    delete image

--- src/clj/picture_gallery/routes/services/gallery.clj
+++ src/clj/picture_gallery/routes/services/gallery.clj
@@ -18,3 +18,7 @@
 (defn list-galleries []
   (ok (db/select-gallery-previews)))
 
+(defn delete-image! [owner thumb-name image-name]
+  (db/delete-file! {:owner owner :name thumb-name})
+  (db/delete-file! {:owner owner :name image-name})
+  (ok {:result :ok}))
